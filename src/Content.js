e4b24e88ee334ec5859d95adc33fab5c624e0a75
LilyBurton <lil@Lilys-MacBook-Air.local>
2024-06-13 17:11:52 +0100

    Brushing up on useState

--- src/Content.js
+++ src/Content.js
@@ -1,14 +1,31 @@
+import { useState } from 'react';
+
 const Content = () => {
+
+    const [name, setName] = useState('Charlie');
+    const [count, setCount] = useState(0);
+
     const handleNamesChange = () => {
-        const names = ['Charlie', 'Vaggie', 'Angel', 'Alastor', 'Nifty', 'Husk'];
-        const int = Math.floor(Math.random() * 6);
-        return names[int];
+        const names = ['Charlie', 'Vaggie', 'Angel', 'Alastor', 'Nifty', 'Husk', 'Lucifer'];
+        const int = Math.floor(Math.random() * 7);
+        setName(names[int]);
+      }
+
+      const handleClick = () => {
+        setCount(count + 1)
+        console.log(count)
+      }
+      const handleClick2 = () => {
+        console.log(count)
       }
     return (
         <main>
-            <p>
-                Hello {handleNamesChange()}!
+            <p onDoubleClick={handleClick}>
+                Hello {name}!
             </p>
+            <button onClick={handleNamesChange}>Change Name!</button>
+            <button onClick={handleClick}>Click Me!</button>
+            <button onClick={handleClick2}>Click Me!</button>
         </main>
     )
 }
