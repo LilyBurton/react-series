9550b5502dee68210f1821133f6002be71991af1
LilyBurton <lil@192.168.1.123>
2024-06-19 16:45:33 +0100

    Add list of items and keys and styling with CSS

--- src/Content.js
+++ src/Content.js
@@ -1,16 +1,45 @@
+import { useState } from 'react';
+import { FaTrashAlt } from 'react-icons/fa'
+
 const Content = () => {
-    const handleNamesChange = () => {
-        const names = ['Charlie', 'Vaggie', 'Angel', 'Alastor', 'Nifty', 'Husk'];
-        const int = Math.floor(Math.random() * 6);
-        return names[int];
+    const [items, setItems] = useState([
+      {
+        id: 1,
+        checked: false,
+        item: "Beef Mince"
+      },
+      {
+        id: 2,
+        checked: false,
+        item: "Peppers"
+      },
+      {
+        id: 3,
+        checked: false,
+        item: "Burger Buns"
       }
+    ]);
+    
+      
     return (
         <main>
-            <p>
-                Hello {handleNamesChange()}!
-            </p>
+          <ul>
+            {items.map((item) => (
+              <li className ='item' key={item.id}>
+                <input
+                  type="checkbox"
+                  checked={item.checked}
+                />
+                <label>{item.item}</label>
+                <FaTrashAlt 
+                  role='button' 
+                  tabIndex='0' />
+              </li>
+            ))}
+          </ul>
+            
         </main>
     )
-}
+  }
 
 export default Content;
